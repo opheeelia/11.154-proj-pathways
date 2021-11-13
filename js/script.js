4af671c968b4266df03f80e7e1f9471bb5ac7ba8
Ophelia <wattinytoffee@gmail.com>
2021-11-13 16:40:12 -0500

    basic scrollytelling template added with some aesthetics, jump link

--- js/script.js
+++ js/script.js
@@ -4,10 +4,36 @@
 
 // PARSE DATA
 // load csv data
-d3.csv("./data.csv", d3.autoType)
-.then(function(data) {
-    // 3. CODE EXECUTED THIRD
-    // use data in functions here
-})
+// d3.csv("./data.csv", d3.autoType)
+// .then(function(data) {
+//     // 3. CODE EXECUTED THIRD
+//     // use data in functions here
+// })
+
+var controller = new ScrollMagic.Controller();
+    
+new ScrollMagic.Scene({
+                        triggerHook: 0.8,
+                        triggerElement: ".scroll-1",
+                        duration: "80%"
+                    })
+    .setClassToggle(".scroll-1", "active") 
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+                        triggerHook: 0.8,
+                        triggerElement: ".scroll-2",
+                        duration: "80%"
+                    })
+    .setClassToggle(".scroll-2", "active") 
+    .addTo(controller);
+                
+new ScrollMagic.Scene({
+                        triggerHook: 0.8,
+                        triggerElement: ".scroll-3",
+                        duration: "80%"
+                    })
+    .setClassToggle(".scroll-3", "active") 
+    .addTo(controller);
 
 // 2. CODE EXECUTED SECOND
